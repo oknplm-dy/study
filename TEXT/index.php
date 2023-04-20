ac1c9878977b28bd1fab48803f68df7aad1d12d7
sang-dongyan <2149016331@qq.com>
2023-04-20 21:52:10 +0800

    人设

---
+++ TEXT/index.php
@@ -0,0 +1,72 @@
+
+<?php
+// $a = 20;
+// switch($a){
+//     case 5:
+//         echo 5;
+//         break;
+//         case 6:
+//             echo 6;
+//             break;
+//             case 7:
+//                 echo 7;
+//                 break;
+//                 case 8:
+//                     echo 8;
+//                     break;
+//                     default:
+//                     echo 'a啥也不是';
+//                     break;
+// }
+
+// if($a == 5){
+//     echo 5;
+// }elseif($a == 6){
+//     echo 6;
+// }elseif($a == 7){
+//     echo 7;
+// }elseif($a == 8){
+//     echo 8;
+// }else{
+//     echo 'b啥也不是';
+// }
+
+// $a = 5;
+// 分步输出
+// $b = $a > 5?'a大于5':'a小于5';
+// echo $b;
+// 下面直接输出(三元运算符用法)
+// $a > 5? print 'a会大于5':print 'a会小于5';
+// 前面判断,后面结果
+// $id_REUEST['ID']?$_REQUEST['ID']:false;
+// .是把两结果拼在一起
+// $a = 'afgrefvaw';
+// $b = 12;
+// $c = 32;
+// echo $a.$b.$c;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ text.php
@@ -0,0 +1,8 @@
+2121561231/*
+ * @Author: error: git config user.name && git config user.email & please set dead value or install git
+ * @Date: 2022-12-06 21:58:32
+ * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
+ * @LastEditTime: 2022-12-06 22:50:17
+ * @FilePath: \.vscoded:\BanGong\XiaoPi\phpstudy_pro\WWW\text.php
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
